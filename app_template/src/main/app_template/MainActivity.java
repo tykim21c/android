8acdc2db47fc3218223d8f5cb0c52a49a3f55c0a
NE10320 <taeyoung.kim@nhnent.com>
2014-12-11 17:15:04 +0900

    test directory commit

--- app_template/src/main/app_template/MainActivity.java
+++ app_template/src/main/app_template/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // 이걸로 교체되어야함.
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
