d50bc1dff8709351a1945706317bdc985614bfa2
NE10320 <taeyoung.kim@nhnent.com>
2014-12-12 11:07:00 +0900

    change master

--- app_template/src/main/java/com/tykim21c/android/app_template/MainActivity.java
+++ app_template/src/main/java/com/tykim21c/android/app_template/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        //이건 또 틀려진다.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
